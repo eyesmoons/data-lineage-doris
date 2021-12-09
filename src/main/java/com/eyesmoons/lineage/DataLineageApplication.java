14163848b1125786ea2f2e864b7006817f3e7d3c
shengy <shengy@shuhaisc.com>
2021-12-09 15:27:08 +0800

    修改包名路径

--- src/main/java/com/eyesmoons/lineage/DataLineageApplication.java
+++ src/main/java/com/eyesmoons/lineage/DataLineageApplication.java
@@ -1,15 +1,18 @@
 package com.eyesmoons.lineage;
 
+import com.eyesmoons.lineage.neo4j.dao.SimpleJpaRepositoryImpl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.data.neo4j.repository.config.EnableNeo4jRepositories;
 
 /**
  * 启动类
  */
 @SpringBootApplication(scanBasePackages = {DataLineageApplication.BASE_PACKAGE}, exclude = {DataSourceAutoConfiguration.class})
-@EntityScan(basePackages = "com.eyesmoons.lineage.event.domain.model")
+@EnableNeo4jRepositories(repositoryBaseClass = SimpleJpaRepositoryImpl.class, basePackages = "com.eyesmoons.lineage.neo4j.dao")
+@EntityScan(basePackages = "com.eyesmoons.lineage.neo4j.domain")
 public class DataLineageApplication {
 
     public final static String BASE_PACKAGE = "com.eyesmoons.lineage.*";
